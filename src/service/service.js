68bfb065a5081a7ef13cecbbcdf0cd7fe1763d40
Sergey <sergeyfilatov.j@gmail.com>
2021-07-25 19:32:06 +0400

    Добавить обработку команд от пользователя

--- src/service/service.js
+++ src/service/service.js
@@ -1,6 +1,18 @@
 'use strict';
 
+const {
+  DEFAULT_COMMAND,
+  USER_ARGV_INDEX,
+  ExitCode
+} = require(`../constants`);
 const {Cli} = require(`./cli`);
 
-Cli[`--help`].run();
+const userArguments = process.argv.slice(USER_ARGV_INDEX);
+const [userCommand] = userArguments;
 
+if (userArguments.length === 0 || !Cli[userCommand]) {
+  Cli[DEFAULT_COMMAND].run();
+  process.exit(ExitCode.success);
+}
+
+Cli[userCommand].run(userArguments.slice(1));
