b2930f21579b022038cf46fa6b204fecef37f6a4
Sergey <sergeyfilatov.j@gmail.com>
2021-09-23 16:33:44 +0400

    Убрать console.log в сервисе для категорий

--- src/service/data-service/category.js
+++ src/service/data-service/category.js
@@ -7,7 +7,6 @@ class CategoryService {
 
   findAll() {
     const categories = this._articles.reduce((acc, article) => {
-      console.log(article);
       article.category.forEach((category) => acc.add(category));
       return acc;
     }, new Set());
