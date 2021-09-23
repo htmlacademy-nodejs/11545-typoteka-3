ed64f0e5900341b6c40a025d2198c7dd7fdb3d61
Sergey <sergeyfilatov.j@gmail.com>
2021-09-23 12:25:07 +0400

    Добавить генерацию комментариев и id записей к моковым данным

--- src/constants.js
+++ src/constants.js
@@ -8,15 +8,19 @@ const ExitCode = {
 };
 const HttpResponseCode = {
   OK: 200,
+  CREATED: 201,
+  BAD_REQUEST: 400,
   NOT_FOUND: 404,
   INTERNAL_SERVER_ERROR: 500,
   FORBIDDEN: 403,
   UNAUTHORIZED: 401,
 };
+const MAX_ID_LENGTH = 6;
 
 module.exports = {
   DEFAULT_COMMAND,
   USER_ARGV_INDEX,
   ExitCode,
   HttpResponseCode,
+  MAX_ID_LENGTH,
 };
